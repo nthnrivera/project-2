f78eeb6a2d32a71f2d7d583ede7e95a339fd764a
CENGIZHAN KARABEL <69279953+cengizhankarabel@users.noreply.github.com>
2021-12-13 16:46:50 -0600

    add missing file

---
+++ ShopBackend/src/main/java/com/example/service/PaymentServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.service;
+
+public class PaymentServiceImpl implements PaymentService{
+
+    // can I call cart information here
+    // I need cart and User information => amount user balance
+    // Do I need one more controller "Payment Controller"
+}
--- ShopBackend/src/main/java/com/example/web/CartController.java
+++ ShopBackend/src/main/java/com/example/web/CartController.java
@@ -1,9 +1,17 @@
 package com.example.web;
 
+import com.example.entity.Cart;
 import com.example.repository.CartRepository;
 import com.example.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class CartController {
@@ -16,4 +24,20 @@ public class CartController {
 
 //    @Autowired
 //    private UserRepository userRepository;
+
+    // GET : /api/todos/{todoId}
+
+    @RequestMapping(
+            method = RequestMethod.GET,
+            value = "/api/cart/{cartId}",
+            produces = {"application/json"}
+    )
+    public ResponseEntity<?> get(@PathVariable(name = "cartId") int cartId) {
+        Optional<Cart> optionalCart = cartRepository.findById(cartId);
+        if (optionalCart.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.ok(optionalCart.get());
+
+    }
 }
