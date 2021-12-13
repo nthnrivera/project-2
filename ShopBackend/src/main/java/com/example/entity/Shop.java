210ac12aaf52520a97250363a50dc0d803830500
remiimartinnn <32273204+remiimartinnn@users.noreply.github.com>
2021-12-13 00:11:21 -0500

    new push
    updated Shop.java code to implement getters and setters, as well as image_path & is_available

--- ShopBackend/src/main/java/com/example/entity/Shop.java
+++ ShopBackend/src/main/java/com/example/entity/Shop.java
@@ -21,5 +21,71 @@ public class Shop {
     private String name;
     private double price;
     private String description;
+    //Revee' added this for product images
+    private String image_path;
+    //Revee' added this for whether an item is still in stock or not
+    private boolean is_available;
+
+        public Shop(int id, String name, double price, String description, String image_path, boolean is_available) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.image_path = image_path;
+        this.is_available = is_available;
+    }
+
+    public Shop() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage_path() {
+        return image_path;
+    }
+
+    public void setImage_path(String image_path) {
+        this.image_path = image_path;
+    }
+
+    public boolean getIs_available() {
+        return is_available;
+    }
+
+    public void setIs_available(boolean is_available) {
+        this.is_available = is_available;
+    }
+
+
 
 }
